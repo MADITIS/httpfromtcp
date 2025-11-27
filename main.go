c877522e5acdad28e2b36428ff10e69f98c373f7
manik <manik6418@gmail.com>
2025-11-27 10:47:32 -0800

    reading 8 bytes

--- main.go
+++ main.go
@@ -13,16 +13,37 @@ func main() {
 
 	}
 
+	// current_line :=
 	buf := make([]byte, 8)
+
+	// read the file (8 bytes at a time) into the bytes slice
+	// get the line until new lines happens
+	// print that line
+	var line []byte
 
 	for {
 		content, err := file.Read(buf)
+		// if (io.EOF == err) {
+		// 	break
+		// }
 		if (err != nil) {
 			break
 		}
+		for i := 0; i < content; i++ {
+			if (buf[i] == '\n') {
+				fmt.Printf("read: %s\n", string(line))
+				line = []byte{}
+			} else {
+				line = append(line, buf[i])
+			}
+		}
 
-		fmt.Printf("read: %s\n", string(buf[:content]))
+
+
 
 	}
+	if len(line) > 0 {
+        fmt.Printf("read: %s\n", string(line))
+    }
 
 }
