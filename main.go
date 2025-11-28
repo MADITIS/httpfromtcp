bcc94efd8718a37077c9210ff25c12856cd137d0
manik <manik6418@gmail.com>
2025-11-27 22:51:54 -0800

    add channels

--- main.go
+++ main.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer file.Close()
+		buf := make([]byte, 8)
+
+		// read the file (8 bytes at a time) into the bytes slice
+		// get the line until new lines happens
+		// print that line
+		var line []byte
+
+		for {
+			content, err := file.Read(buf)
+			// if (io.EOF == err) {
+			// 	break
+			// }
+			if err != nil {
+				break
+			}
+			for i := 0; i < content; i++ {
+				if buf[i] == '\n' {
+					ch <- string(line)
+					line = []byte{}
+				} else {
+					line = append(line, buf[i])
+				}
+			}
+
+		}
+		if len(line) > 0 {
+			ch <- string(line)
+		}
+	}()
+
+	return ch
+}
 
 func main() {
 	file, err := os.Open("./messages.txt")
@@ -13,37 +53,10 @@ func main() {
 
 	}
 
-	// current_line :=
-	buf := make([]byte, 8)
+	current_line := getLinesChannel(file)
 
-	// read the file (8 bytes at a time) into the bytes slice
-	// get the line until new lines happens
-	// print that line
-	var line []byte
-
-	for {
-		content, err := file.Read(buf)
-		// if (io.EOF == err) {
-		// 	break
-		// }
-		if (err != nil) {
-			break
-		}
-		for i := 0; i < content; i++ {
-			if (buf[i] == '\n') {
-				fmt.Printf("read: %s\n", string(line))
-				line = []byte{}
-			} else {
-				line = append(line, buf[i])
-			}
-		}
-
-
-
-
+	for line := range current_line {
+		fmt.Printf("read: %s\n", line)
 	}
-	if len(line) > 0 {
-        fmt.Printf("read: %s\n", string(line))
-    }
 
 }
